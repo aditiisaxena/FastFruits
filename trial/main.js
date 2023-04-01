9b49d57ee630c8d8eed431e4bba8d2edd8086ff0
aditi <aditi21371@iiitd.ac.in>
2023-04-02 00:19:35 +0530

    Frontend kiya uwu

---
+++ trial/main.js
@@ -0,0 +1,18 @@
+if(){
+    some==SVGFEDropShadowElement
+    if(){
+        if(){
+            function something(){
+                Array.array.array.forEach(element => {
+                    
+                });
+            }
+        }
+        else{
+
+        }
+    }
+    else{
+
+    }
+}
